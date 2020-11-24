5ae0e3eaea6e6888d7daa674bfd84f9800133f70
Charlychee <charles.lychee@gmail.com>
2020-11-23 23:32:15 -0800

    Fixed recursive calls to not repeat traversed nodes

--- modules/courses.py
+++ modules/courses.py
@@ -13,24 +13,52 @@ with open(abs_file_path) as f:
     COURSE_DICT = json.load(f)
 
 
+def remove_star(string):
+    """
+    Removes star from end of string if it exists and returns the resulting string
+    :param string: The string to have * removed
+    :return: string: The modified string with * removed
+    """
+    if string[-1] == '*':
+        string = string[:-1]
+    return string
+
+
 def find_next_node(graph, course, complete):
     """
     Adds all the prerequisite courses to the graph and their prerequisites, recursively.
     :param graph: The pydot.Dot object which holds the tree
-    :param course: The course name which we are adding its prerequisite classes to
+    :param course: The course name which we are adding its prerequisite courses to
     :param complete: The list of courses that have already been added to the tree. This is to prevent infinite loops.
     """
-    if COURSE_DICT[course] is None:
+
+    # Add course to complete list to signify traversal and to not traverse again
+    complete.append(course)
+    try:
+        if COURSE_DICT[course] is None:
+            # Base case
+            return
+    except KeyError:
+        # If course isn't in dictionary, assume no prereq
         return
+
     for prereq in COURSE_DICT[course]:
         if isinstance(prereq, list):
             # OR COURSES
             for or_req in prereq:
                 add_course(graph, or_req, course, 'blue')
+                or_req = remove_star(or_req)
+                if or_req in complete:
+                    # Already traversed node, skip traversal
+                    continue
                 find_next_node(graph, or_req, complete)
         else:
             # AND COURSES
             add_course(graph, prereq, course, 'red')
+            prereq = remove_star(prereq)
+            if prereq in complete:
+                # Already traversed node, skip traversal
+                continue
             find_next_node(graph, prereq, complete)
 
 
@@ -58,7 +86,6 @@ def get_prereqs(course_name):
     :param course_name: A string with the name of the course you are looking prereqs for
     :return graph: The pydot.Dot object which holds the tree of the course and all its prereqs
     """
-
     graph = pydot.Dot(graph_type='digraph')
     target = pydot.Node(course_name)
     graph.add_node(target)
